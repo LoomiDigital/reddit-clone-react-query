74899d89b8166215bea9385f0b1e215e6b485a3e
Iñigo Villalba <inigo@loomi.io>
2023-07-18 12:59:40 +0200

    Add post feed component tests

---
+++ __tests__/feed.test.tsx
@@ -0,0 +1,42 @@
+import { render } from "@testing-library/react";
+
+import { mockPostConnection } from "@d20/mocks/getPosts";
+
+import Feed from "@d20/components/Feed";
+import PostCard from "@d20/components/PostCard";
+import { PostLoader } from "@d20/components/Loaders";
+
+jest.mock("@d20/Components/PostCard");
+jest.mock("@d20/Components/Loaders", () => ({
+  PostLoader: jest.fn(),
+}));
+
+describe("Feed component", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should handle the loading state", async () => {
+    render(
+      <Feed
+        loading={true}
+        loadingRef={() => {}}
+        posts={mockPostConnection.edges}
+      />
+    );
+
+    expect(PostLoader).toHaveBeenCalled();
+  });
+
+  it("should render the correct number of posts", async () => {
+    render(
+      <Feed
+        loading={false}
+        loadingRef={() => {}}
+        posts={mockPostConnection.edges}
+      />
+    );
+
+    expect(PostCard).toHaveBeenCalledTimes(2);
+  });
+});
---
+++ mocks/getPosts.ts
@@ -0,0 +1,186 @@
+import {
+  GetPostsDocument,
+  GetPostsQuery,
+  PostConnection,
+} from "@d20/generated/graphql";
+
+export const mockPostsResponse = {
+  result: {
+    data: {
+      posts: {
+        __typename: "PostConnection",
+        edges: [
+          {
+            __typename: "PostEdge",
+            cursor: "YXJyYXljb25uZWN0aW9uOjA=",
+            node: {
+              __typename: "Post",
+              id: 1,
+              title: "Test Post",
+              body: "Test Content",
+              image: "http://placehold.it/300x300",
+              username: "Buck",
+              votes: [
+                {
+                  __typename: "Vote",
+                  upvote: true,
+                  username: "Buck",
+                  post_id: 1,
+                },
+              ],
+              subreddit_id: 1,
+              subreddit_topic: "test",
+              created_at: "2021-08-15T20:00:00.000Z",
+            },
+          },
+          {
+            __typename: "PostEdge",
+            cursor: "YXJyYXljb25uZWN0aW9uOjA=",
+            node: {
+              __typename: "Post",
+              id: 2,
+              title: "Test Post",
+              body: "Test Content",
+              username: "Buck",
+              image: "http://placehold.it/300x300",
+              votes: [
+                {
+                  __typename: "Vote",
+                  upvote: true,
+                  username: "Buck",
+                  post_id: 2,
+                },
+              ],
+              subreddit_id: 2,
+              subreddit_topic: "test",
+              created_at: "2021-08-15T20:00:00.000Z",
+            },
+          },
+        ],
+        pageInfo: {
+          __typename: "PageInfo",
+          hasNextPage: false,
+          endCursor: "YXJyYXljb25uZWN0aW9uOjA=",
+        },
+      },
+    },
+  },
+};
+
+export const mockPosts: GetPostsQuery = {
+  posts: {
+    __typename: "PostConnection",
+    edges: [
+      {
+        __typename: "PostEdge",
+        cursor: "YXJyYXljb25uZWN0aW9uOjA=",
+        node: {
+          __typename: "Post",
+          id: 1,
+          title: "Test Post",
+          body: "Test Content",
+          image: "http://placehold.it/300x300",
+          username: "Buck",
+          votes: [
+            {
+              __typename: "Vote",
+              upvote: true,
+              username: "Buck",
+              post_id: 1,
+            },
+          ],
+          subreddit_id: 1,
+          subreddit_topic: "test",
+          created_at: "2021-08-15T20:00:00.000Z",
+        },
+      },
+      {
+        __typename: "PostEdge",
+        cursor: "YXJyYXljb25uZWN0aW9uOjA=",
+        node: {
+          __typename: "Post",
+          id: 2,
+          title: "Test Post",
+          body: "Test Content",
+          username: "Buck",
+          image: "http://placehold.it/300x300",
+          votes: [
+            {
+              __typename: "Vote",
+              upvote: true,
+              username: "Buck",
+              post_id: 2,
+            },
+          ],
+          subreddit_id: 2,
+          subreddit_topic: "test",
+          created_at: "2021-08-15T20:00:00.000Z",
+        },
+      },
+    ],
+    pageInfo: {
+      __typename: "PageInfo",
+      hasNextPage: false,
+      endCursor: "YXJyYXljb25uZWN0aW9uOjA=",
+    },
+  },
+};
+
+export const mockPostConnection: PostConnection = {
+  __typename: "PostConnection",
+  edges: [
+    {
+      __typename: "PostEdge",
+      cursor: "YXJyYXljb25uZWN0aW9uOjA=",
+      node: {
+        __typename: "Post",
+        id: 1,
+        title: "Test Post",
+        body: "Test Content",
+        image: "http://placehold.it/300x300",
+        username: "Buck",
+        votes: [
+          {
+            __typename: "Vote",
+            upvote: true,
+            username: "Buck",
+            post_id: 1,
+          },
+        ],
+        subreddit_id: 1,
+        subreddit_topic: "test",
+        created_at: "2021-08-15T20:00:00.000Z",
+      },
+    },
+    {
+      __typename: "PostEdge",
+      cursor: "YXJyYXljb25uZWN0aW9uOjA=",
+      node: {
+        __typename: "Post",
+        id: 2,
+        title: "Test Post",
+        body: "Test Content",
+        username: "Buck",
+        image: "http://placehold.it/300x300",
+        votes: [
+          {
+            __typename: "Vote",
+            upvote: true,
+            username: "Buck",
+            post_id: 2,
+          },
+        ],
+        subreddit_id: 2,
+        subreddit_topic: "test",
+        created_at: "2021-08-15T20:00:00.000Z",
+      },
+    },
+  ],
+  pageInfo: {
+    __typename: "PageInfo",
+    hasPreviousPage: false,
+    hasNextPage: false,
+    startCursor: "YXJyYXljb25uZWN0aW9uOjA=",
+    endCursor: "YXJyYXljb25uZWN0aW9uOjA=",
+  },
+};
